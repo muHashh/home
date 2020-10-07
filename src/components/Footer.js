774d330e6bb0189f2ceb01cfc1d2b008b548052e
Hassan Shahid <muhashh@protonmail.com>
2020-10-07 13:20:10 +0100

    Add WIP stamp in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer transparent">
+      Site construction in progress
+      <br/><br/>
       Made by me using React and Bootstrap
     </div>
   );
