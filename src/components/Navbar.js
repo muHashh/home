0e4b091eb1779b59e0b8804ca5f043946798a08d
Hassan Shahid <muhashh@protonmail.com>
2020-10-07 01:14:35 +0100

    remove brackets from home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,7 +19,7 @@ const Navbar = (props) => {
       } `}
     >
       <a className="navbar-brand" href={process.env.PUBLIC_URL + "/#home"}>
-        {"<Home>"}
+        {"Home"}
       </a>
       <button
         className="navbar-toggler"
